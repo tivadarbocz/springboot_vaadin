bf6046558ec2340302b658cc52d711528bb95d22
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2016-11-04 13:08:06 +0100

    #11 -Session destroy after 5 min - vaadin heartbeat 30 sec

--- src/main/java/hu/tb/security/CustomServlet.java
+++ src/main/java/hu/tb/security/CustomServlet.java
@@ -1,14 +1,18 @@
 package hu.tb.security;
 
-import com.vaadin.server.CustomizedSystemMessages;
-import com.vaadin.server.SystemMessagesProvider;
+import com.vaadin.server.*;
 import com.vaadin.spring.server.SpringVaadinServlet;
+import hu.tb.util.PropertyContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 
 @Component("vaadinServlet")
-public class CustomServlet extends SpringVaadinServlet {
+public class CustomServlet extends SpringVaadinServlet implements SessionInitListener, SessionDestroyListener {
+
+    private final Logger logger = LoggerFactory.getLogger(CustomServlet.class);
 
     @Override
     protected void servletInitialized() throws ServletException {
@@ -18,10 +22,23 @@ public class CustomServlet extends SpringVaadinServlet {
             // Don't show any messages, redirect immediately to the session expired URL
             messages.setSessionExpiredNotificationEnabled(false);
             // Force a logout to also end the HTTP session and not only the Vaadin session
-            messages.setSessionExpiredURL("logout");
+            messages.setSessionExpiredURL(PropertyContainer.getServerContextPath().concat( "/logout"));
             // Don't show any message, reload the page instead
             messages.setCommunicationErrorNotificationEnabled(false);
             return messages;
         });
+
+        getService().addSessionInitListener(this);
+        getService().addSessionDestroyListener(this);
+    }
+
+    @Override
+    public void sessionDestroy(SessionDestroyEvent sessionDestroyEvent) {
+        logger.info("Session destroyed");
+    }
+
+    @Override
+    public void sessionInit(SessionInitEvent sessionInitEvent) throws ServiceException {
+        logger.info("Session created");
     }
 }
